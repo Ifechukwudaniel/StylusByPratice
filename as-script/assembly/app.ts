577dfe1c3a39fb9807fc693f9231d0482c84b0ed
Angry Coding <mailidanielifechukwu@gmail.com>
2024-09-09 13:35:54 +0000

    Stylus in assembly

---
+++ as-script/assembly/app.ts
@@ -0,0 +1,14 @@
+import { i32ToBytes, bytesToI32 } from './utils';
+/**
+ * Main function of your program
+ * @dev Receives the input of bytes in Uint8Array. Result must also be sent in bytes wrapped in Uint8Array
+ *
+ * @param input bytes in Uint8Array
+ * @returns bytes in Uint8Array
+ */
+
+
+export function main(input:Uint16Array) :Uint16Array {
+    return  new Uint16Array(10).fill(10)
+}
+
