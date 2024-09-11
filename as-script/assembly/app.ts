ec9596007524f14bbccfc2471fa1ec6666c3d105
Angry Coding <mailidanielifechukwu@gmail.com>
2024-09-11 15:36:30 +0000

    Implemented bytes to i32

--- as-script/assembly/app.ts
+++ as-script/assembly/app.ts
@@ -16,76 +16,79 @@ function usqrt(n: u32): u32 {
  * (Based on t-katsumura's implementation: https://github.com/t-katsumura/webassembly-examples-eratosthenes)
  * (Sieve of Eratosthenes explanation: https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes)
  */
+
+
 function getMaxPrimeUpTo(number: i32): i32 {
     // 0 here is meant to be interpreted as an error
     if (number < 2) {
-        return 0;
+      return 0;
     }
-
+  
     // If 2 is passed, return 2
     if (number == 2) {
-        return 2;
+      return 2;
     }
-
+  
     // Length of the sieve array
     const length = (number - 1) / 2;
-
+  
     // Square root (max)
     const maxNumberToCheck = usqrt(number);
-
+  
     // Sieve array (starting from 3, without multiples of 2) [3, 5, 7, 9, ...]
     const sieve = new StaticArray<bool>(length).fill(true);
-
+  
     // Coordinates of the Sieve
     let x: u32;
     let y: u32;
-
+  
     for (let i = 0; i < length; i++) {
-        // Next number to check multiples for
-        x = 2 * (i + 1) + 1;
-
-        // No need to check multiples for numbers that are greater than
-        // the square root of the upper limit
-        if (x > maxNumberToCheck) {
+      // Next number to check multiples for
+      x = 2 * (i + 1) + 1;
+  
+      // No need to check multiples for numbers that are greater than
+      // the square root of the upper limit
+      if (x > maxNumberToCheck) {
         break;
-        }
-
-        // Checking multiples of x
-        for (let j = i + 1; j < length; j++) {
+      }
+  
+      // Checking multiples of x
+      for (let j = i + 1; j < length; j++) {
         if (!unchecked(sieve[j])) {
-            continue;
+          continue;
         }
-
+  
         // Next multiple candidate
         y = 2 * (j + 1) + 1;
-
+  
         // Candidate is multiple of x (not prime then)
         if (y % x == 0) {
-            unchecked((sieve[j] = false));
-
+          unchecked((sieve[j] = false));
         }
+      }
     }
-
+  
     // Get the highest prime number
     let max_val: u32 = 2;
     for (let i = length - 1; i >= 0; i--) {
-        if (unchecked(sieve[i])) {
+      if (unchecked(sieve[i])) {
         max_val = 2 * (i + 1) + 1;
         break;
-        }
+      }
     }
     return max_val;
-    }
-}
+  }
 
 /**
  * Main function of your program
- * @dev Receives the input of bytes in Uint8Array. Result must also be sent in bytes wrapped in Uint8Array
+Receives the input of bytes in Uint8Array. Result must also be sent in bytes wrapped in Uint8Array
  *
  * @param input bytes in Uint8Array
  * @returns bytes in Uint8Array
 **/
 
 export function main(input:Uint8Array) :Uint8Array {
-    return input;
+  const maxNumber = bytesToI32(input);
+  const maxPrime = getMaxPrimeUpTo(maxNumber);
+  return i32ToBytes(maxPrime);
 }
